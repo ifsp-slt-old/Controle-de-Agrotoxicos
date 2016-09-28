15f2e9a2d68880a561e1f7d709639f60c1e24528
Eduardo Pedroso <drdpedroso@gmail.com>
2016-09-28 19:46:58 -0300

    Pages flux working with root folder

--- views/partials/_header.php
+++ views/partials/_header.php
@@ -1,6 +1,6 @@
 <nav class="nav">
   <div class="nav-left">
-    <a class="nav-item is-active" href="#">
+    <a class="nav-item is-active" href="/controle-agrotoxicos">
       Home
     </a>
   </div>
@@ -12,13 +12,13 @@
   </span>
 
   <div class="nav-right nav-menu">
-    <a class="nav-item" href="../controle-agrotoxicos/views/new-entry.php">
+    <a class="nav-item" href="/controle-agrotoxicos/views/new-entry.php">
       Nova Entrada
     </a>
-    <a class="nav-item" href="views/new-dropoff.php">
+    <a class="nav-item" href="/controle-agrotoxicos/views/new-dropoff.php">
       Nova Saida
     </a>
-    <a class="nav-item" href="views/balance.php">
+    <a class="nav-item" href="/controle-agrotoxicos/views/balance.php">
       Relatorios
     </a>
 
