b8b462a6638fcff150f251b39be2ecccfbc0cb52
Eduardo Pedroso <drdpedroso@gmail.com>
2016-10-21 10:49:07 -0200

    Adding Usuario attributes

--- models/Usuario.php
+++ models/Usuario.php
@@ -1,5 +1,17 @@
 <?php
   class Usuario {
 
+    private $nomeUsuario;
+    private $loginUsuario;
+    private $senhaUsuario;
+    private $permissaoUsuario;
+
+    function __construct($nomeUsuario, $loginUsuario, $senhaUsuario, $permissaoUsuario) {
+      $this->nomeUsuario = $nomeUsuario;
+      $this->loginUsuario = $loginUsuario;
+      $this->senhaUsuario = $senhaUsuario;
+      $this->permissaoUsuario = $permissaoUsuario;
+    }
+
   }
 ?>
