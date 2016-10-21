e0b5e6faea415fdc17ce9b08e4937851819763e0
Eduardo Pedroso <drdpedroso@gmail.com>
2016-10-21 10:17:05 -0200

    Adding Requisicao attributes

--- models/Requisicao.php
+++ models/Requisicao.php
@@ -1,5 +1,31 @@
 <?php
   class Requisicao{
 
+    private $tipoRequisicao;
+    private $dataRequisicao;
+    private $principioAtivoReq;
+    private $concentracaoReq;
+    private $formulacaoReq;
+    private $undMedidaReq;
+    private $qtdAplicacaoReq;
+    private $nfNumEntradaReq;
+    private $tipoEmbalagemReq;
+    private $qtdEmbalagemReq;
+
+    function __construct($tipoRequisicao, $dataRequisicao, $principioAtivoReq, $concentracaoReq,
+                         $formulacaoReq, $undMedidaReq, $qtdAplicacaoReq, $nfNumEntradaReq, $tipoEmbalagemReq, $qtdEmbalagemReq) {
+                           
+      $this->tipoRequisicao = $tipoRequisicao;
+      $this->dataRequisicao = $dataRequisicao;
+      $this->principioAtivoReq = $principioAtivoReq;
+      $this->concentracaoReq = $concentracaoReq;
+      $this->formulacaoReq = $formulacaoReq;
+      $this->undMedidaReq = $undMedidaReq;
+      $this->qtdAplicacaoReq = $qtdAplicacaoReq;
+      $this->nfNumEntradaReq = $nfNumEntradaReq;
+      $this->tipoEmbalagemReq = $tipoEmbalagemReq;
+      $this->qtdEmbalagemReq = $qtdEmbalagemReq;
+    }
+
   }
 ?>
