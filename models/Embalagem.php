55de7bc7d1703a86ad26e6d477a5329f8c4a5e8a
Eduardo Pedroso <drdpedroso@gmail.com>
2016-10-21 09:57:34 -0200

    Adding Embalagem attributes

--- models/Embalagem.php
+++ models/Embalagem.php
@@ -1,5 +1,15 @@
 <?php
   class Embalagem {
 
+    private $tipoEmbalagem;
+    private $undMedidaEmbalagem;
+    private $qtdMedidaEmbalagem;
+
+    function __construct($tipoEmbalagem, $undMedidaEmbalagem, $qtdMedidaEmbalagem) {
+      $this->tipoEmbalagem = $tipoEmbalagem;
+      $this->undMedidaEmbalagem = $undMedidaEmbalagem;
+      $this->qtdMedidaEmbalagem = $qtdMedidaEmbalagem;
+    }
+
   }
 ?>
