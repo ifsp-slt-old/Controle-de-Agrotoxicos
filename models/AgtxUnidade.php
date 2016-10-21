65838dfee4ca667cb7ad4022ae3824d6e597f0ae
Eduardo Pedroso <drdpedroso@gmail.com>
2016-10-21 09:51:15 -0200

    Adding AgtxUnidade atributtes

--- models/AgtxUnidade.php
+++ models/AgtxUnidade.php
@@ -1,5 +1,23 @@
 <?php
   class AgtxUnidade {
 
+    private $nomeComercialAgtx;
+    private $classeAplicacaoAgtx;
+    private $principioAtivoAgtx;
+    private $concentracaoAgtx;
+    private $formulacaoAgtx;
+    private $statusAgtx;
+
+    function __construct($nomeComercialAgtx, $classeAplicacaoAgtx, $principioAtivoAgtx,
+                         $concentracaoAgtx, $formulacaoAgtx, $statusAgtx) {
+
+      $this->nomeComercialAgtx = $nomeComercialAgtx; //Nome comercial do agrotoxico
+      $this->classeAplicacaoAgtx = $classeAplicacaoAgtx; //Classe de aplicaçao do agrotoxico
+      $this->principioAtivoAgtx = $principioAtivoAgtx; //Principio Ativo
+      $this->concentracaoAgtx = $concentracaoAgtx; //Concentraçao em ml/2
+      $this->formulacaoAgtx = $formulacaoAgtx; //Formulaçao
+      $this->statusAgtx = $statusAgtx; //Status atual
+    }
+
   }
 ?>
