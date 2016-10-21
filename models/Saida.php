834c4c37294206bc94bce13a8997c76a7893611d
Eduardo Pedroso <drdpedroso@gmail.com>
2016-10-21 10:46:39 -0200

    Adding Saida attributes

--- models/Saida.php
+++ models/Saida.php
@@ -1,5 +1,15 @@
 <?php
   class Saida {
 
+    private $tipoSaida;
+    private $dataSaida;
+    private $pesoSaida;
+
+    function __construct($tipoSaida, $dataSaida, $pesoSaida) {
+      $this->tipoSaida = $tipoSaida;
+      $this->dataSaida = $dataSaida;
+      $this->pesoSaida = $pesoSaida;
+    }
+
   }
 ?>
