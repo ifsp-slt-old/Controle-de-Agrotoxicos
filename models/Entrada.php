befae8e0f233315439993b2107f0a5a6fe7a1022
Eduardo Pedroso <drdpedroso@gmail.com>
2016-10-21 10:04:11 -0200

    Adding Entrada attributes

--- models/Entrada.php
+++ models/Entrada.php
@@ -1,5 +1,24 @@
 <?php
   class Entrada {
-    
+    private $receitaEntrada;
+    private $nfNumEntrada;
+    private $dataNfEntrada;
+    private $dataFabEntrada;
+    private $dataValEntrada;
+    private $dataEntrada;
+    private $qtdAtualEntrada;
+    private $statusUsoEntrada;
+
+    function __construct($receitaEntrada, $nfNumEntrada, $dataNfEntrada, $dataFabEntrada, $dataValEntrada,
+                         $dataEntrada, $qtdAtualEntrada, $statusUsoEntrada) {
+      $this->receitaEntrada = $receitaEntrada;
+      $this->nfNumEntrada = $nfNumEntrada;
+      $this->dataNfEntrada = $dataNfEntrada;
+      $this->dataFabEntrada = $dataFabEntrada;
+      $this->dataValEntrada = $dataValEntrada;
+      $this->dataEntrada = $dataEntrada;
+      $this->qtdAtualEntrada = $qtdAtualEntrada;
+      $this->statusUsoEntrada = $statusUsoEntrada;
+    }
   }
 ?>
