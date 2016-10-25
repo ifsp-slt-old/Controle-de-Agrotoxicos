0dacc1a7c76370dd71f40194777df741d734462b
AndreFOliveira <andreoliveira.96@hotmail.com>
2016-10-25 10:58:38 -0200

    Login-controller.php
    Alterações para recebimento de dados e comunicação basica com SQL

--- controllers/login-controller.php
+++ controllers/login-controller.php
@@ -1,5 +1,42 @@
 <?php
   class LoginController {
+	  
+	  /*------------
+	  @_AndreOliveira
+	  @_EduardoPedroso
+	  */
+	  
+	
+	 
+	public function do_login($email, $senha){
+		
+		//tratar os dados se necessario
+		//MD5 PARA SENHA, STRTOLOWER PARA EMAIL
+		//E POR AI VAI
+		
+		do_select($email, $senha) 
+	}
+	
+	public function do_select($email, $senha){
+	
+	//chama conexao
+
+	$sql = sprintf("SELECT * from usuario WHERE loginUsuario = '%s' AND senhaUsuario = '%s'", $email, $senha);
+	//executa a query ($sql->execute();)
+	
+	if(!$sql){
+	return false; //OU ERRO, CASO QUISER TRATAÇÃO ESPECIFICA
+	}else{
+		//COMO OBRIGATORIAMENTE O SELECT TRAZ APENAS 1 REGISTRO
+    //PODEMOS SETAR FIXAMENTE AS VARIAVEIS
+    $email_bd = $sql['loginUsuario']; //CASO NÃO FUNCIONE, MUDE PARA $sql->LoginUsuario;
+    $pass_bd =  $sql['senhaUsuario'];
+		header("Location: ../views/pagina.php"); //DIRECIONA PARA A INDEX DO SITE (ALTERAR CAMINHO)
+
+	}	
+		
+	}
+	   
     
   }
 ?>
