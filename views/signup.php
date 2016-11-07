4a7d908748c9602c6d0998b3522a5ff448133387
Eduardo Pedroso <drdpedroso@gmail.com>
2016-11-07 11:40:07 -0200

    Creating signup form

--- views/signup.php
+++ views/signup.php
@@ -11,13 +11,54 @@
 			<span class="title"><h3>Cadastro</h3></span>
       <form action="" method="POST" class="margin-top-10">
         <p class="control">
-          <input class="input" type="text" placeholder="Loading input">
+          <input class="input" type="text" placeholder="Nome" name="nome">
         </p>
-        <p class="control">
-          <textarea class="textarea" placeholder="Loading textarea"></textarea>
+				<p class="control">
+          <input class="input" type="text" placeholder="Login" name="login">
         </p>
+				<p class="control">
+					<input class="input" type="password" placeholder="Senha" name="senha">
+				</p>
+				<p class="control">
+					<input class="input" type="password" placeholder="Confirmar senha" name="senha_conf">
+				</p>
+				<div class="columns center">
+					<label class="radio column">
+				    <input type="radio" name="permissao" value="O">
+				    Gerente operacional
+				  </label>
+				  <label class="radio column">
+				    <input type="radio" name="permissao" value="U">
+				    Gerente de unidade
+				  </label>
+					<label class="radio column">
+						<input type="radio" name="permissao" value="E">
+						Encarregado de estoque
+					</label>
+					<label class="radio column">
+						<input type="radio" name="permissao" value="A">
+						Encarregado de aplicação
+					</label>
+				</div>
+				<div class="columns margin-top-20">
+					<button class="button is-info column" type="submit">
+						Cadastrar
+					</button>
+				</div>
       </form>
     </div>
-
 	</body>
+
+	<?php
+		include_once('../controllers/signup-controller.php');
+		if(!empty($_POST)){
+			if($_POST["senha"] == $_POST["senha_conf"]){
+				$controller = new SignupController();
+				$controller->do_signup($_POST["nome"], $_POST["login"], $_POST["senha"], $_POST["senha_conf"], $_POST["permissao"]);
+			} else {
+				echo "<span style='color:red'> Senha e Confirmação de senhas precisam ser iguais.</span>";
+			}
+
+		}
+	?>
 </html>
