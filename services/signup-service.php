4d37743c17816d9d090d75350b6878a0ce713763
AndreFOliveira <andreoliveira.96@hotmail.com>
2016-11-08 10:16:00 -0200

    Update Signup - Query OK

--- services/signup-service.php
+++ services/signup-service.php
@@ -1,9 +1,20 @@
 <?php
   class SignupService{
-
+    
+    function __construct(){
+      try {
+          //Faz conexao com o banco
+          $this->dbh = new PDO("mysql:host=127.0.0.1;port=8889;dbname=ctrl_agrotoxicos", 'root', 'root');
+      } catch (PDOException $e) {
+          print "Error!: " . $e->getMessage() . "<br/>";
+          die();
+      }
+    }
+	
 
     public function do_signup($nome, $login, $senha, $permissao){
-      echo $nome;
+       $sql = $this->dbh->query("INSERT INTO usuario(nomeUsuario, loginUsuario, senhaUsuario, permissaoUsuario) VALUES ('$nome', '$login', '$senha', '$permissao')");
+       $this->dbh = null;
     }
   }
 ?>
