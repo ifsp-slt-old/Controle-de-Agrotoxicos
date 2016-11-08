c9b2f9625c4f3e4cee98cfd3fa66a457241e6e64
AndreFOliveira <andreoliveira.96@hotmail.com>
2016-11-08 10:24:11 -0200

    Update new-entry-controller

--- controllers/new-entry-controller.php
+++ controllers/new-entry-controller.php
@@ -1,5 +1,18 @@
 <?php
+	
+  include_once("../services/entry-service.php");
+  
   class NewEntryController{
-
+	  
+	  public function do_new_entry($receitaEntrada, $nfNumEntrada, $dataNfEntrada, $dataFabEntrada, $dataValEntrada, $dataEntrada, $qtdAtualEntrada,
+	  $stattusUsoEntrada){
+	
+   	  $service = new EntryService();
+      $service->new_entry($receitaEntrada, $nfNumEntrada, $dataNfEntrada, $dataFabEntrada, $dataValEntrada, $dataEntrada, $qtdAtualEntrada,
+	  $stattusUsoEntrada);
+	  
+	  }
+	  
+	  
   }
 ?>
