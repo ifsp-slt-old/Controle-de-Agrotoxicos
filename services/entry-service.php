fcd7ccddeb62842be00624f464441f9e188f0f7c
AndreFOliveira <andreoliveira.96@hotmail.com>
2016-11-17 08:39:53 -0200

    Criadas funções da parte de entrada

--- services/entry-service.php
+++ services/entry-service.php
@@ -27,6 +27,81 @@
      
     }
 		
+
+   public function new_entry_embalagem($tipoEmbalagem, $unMedidaEmbalagem, $qtdMedidaEmbalagem){
+      //realiza query
+      $sql = $this->dbh->query("INSERT INTO embalagem(tipoEmbalagem, unMedidaEmbalagem, qtdMedidaEmbalagem) VALUES ('$tipoEmbalagem', '$unMedidaEmbalagem', '$qtdMedidaEmbalagem')");
+     // $result = $sql->fetch();
+      $this->dbh = null;
+     
+    }
+	
+   public function new_entry_fornecedor($nomeFornecedor){
+      //realiza query
+      $sql = $this->dbh->query("INSERT INTO fornecedor(nomeFornecedor) VALUES ('$nomeFornecedor')");
+     // $result = $sql->fetch();
+      $this->dbh = null;
+     
+    }
+		
+   public function new_entry_fabricante($nomeFabricante){
+      //realiza query
+      $sql = $this->dbh->query("INSERT INTO fabricante(nomeFabricante) VALUES ('$nomeFabricante')");
+     // $result = $sql->fetch();
+      $this->dbh = null;
+     
+    }
+
+    /*
+    	select para exibição na pagina com os agrotoxicos ja cadastrados, trazendo seus respectivos fornecedores,
+	embalagens e fabricantes
+    */
+    public function new_entry_get_info_agtx(){
+      //realiza query
+      $sql = $this->dbh->query("SELECT agtxunidade.* FROM agtxunidade");
+      $result = $sql->fetch();
+      $this->dbh = null;
+     
+    }
+		
+	/*
+		Abaixo, queries para trazer cada um dos aliados do agrotoxico (embarcador, fornecedor e embalagem)
+		de acordo com o ID
+		
+		A idéia é quando o usuario selecionar algum agrotoxico que esteja no campo de seleção, ele vai carregar o agrotoxico
+		trazendo os respectivos ID's dessas tabelas, se existirem. Ai é possivel rodar as queries e pegar os nomes.
+		
+		Se não existir, usa-se as queries de INSERT acima para inclusão, e automaticamente pega-se o id inserido.
+	*/
+
+    public function new_entry_get_info_emb($id){
+      //realiza query
+      $sql = $this->dbh->query("SELECT embalagem.* FROM embalagem
+      WHERE idEmbalagem = '$id'");
+      $result = $sql->fetch();
+      $this->dbh = null;
+     
+    }
+
+    public function new_entry_get_info_for($id){
+      //realiza query
+      $sql = $this->dbh->query("SELECT fornecedor.* FROM fornecedor
+      WHERE idFornecedor = '$id'");
+      $result = $sql->fetch();
+      $this->dbh = null;
+     
+    }
+   
+    public function new_entry_get_info_fab($id){
+      //realiza query
+      $sql = $this->dbh->query("SELECT fabricante.* FROM fabricante
+      WHERE idFabricante = '$id'");
+      $result = $sql->fetch();
+      $this->dbh = null;
+     
+    }
+		
+  
 		
 		
 		
