057c78b77f9fca7858b07334da5ce49fd0b71d0b
Eduardo Pedroso <drdpedroso@gmail.com>
2016-12-01 20:08:16 -0200

    Terminando tela de ajuda

--- views/help.php
+++ views/help.php
@@ -50,15 +50,29 @@
       </div>
     </article>
   </div>
-   <div class="box margin-top-20">
+  <div class="box margin-top-20">
     <article class="media">
       <div class="media-content">
         <div class="content">
           <p>
-            <strong>Como cadastrar um novo Fabricante?</strong>
+            <strong>Como cadastrar um novo Usuario?</strong>
             <br>
-            Para cadastrar um novo agrotoxico você precisa ter permissão de nivel E.
-            Com a permissâo concedida, basta ir até o menu superior e escolher a opção de Novo Fabricante. Após isso basta concluir o cadastro preenchendo o formulario e clicando em Criar Fabricante.
+            Para cadastrar um novo usuario você precisa ter permissão de nivel U ou O.
+            Com a permissâo concedida, basta ir até o menu superior e escolher a opção Cadastrar. Após isso basta concluir o cadastro preenchendo o formulario, selecionando o nivel de permissão do usuario a ser cadastrado e clicando em Cadastrar.
+          </p>
+        </div>
+      </div>
+    </article>
+  </div>
+  <div class="box margin-top-20">
+    <article class="media">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>Como visualizar os Relatorios?</strong>
+            <br>
+            Para visualizar relatorios você precisa ter permissão de nivel U ou O.
+            Com a permissâo concedida, basta ir até o menu superior e escolher a opção Relatorios. Após isso basta escolher o filtro de data e clicar em filtrar. Uma tabela deve aparecer logo abaixo do texto 'Entrada'.
           </p>
         </div>
       </div>
